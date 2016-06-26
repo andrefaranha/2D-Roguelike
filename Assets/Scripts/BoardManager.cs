1af0df1624d4ada592ba6fca63099ad5375f90de
Afaranha <andre.f.aranha@gmail.com>
2016-06-26 13:37:53 -0300

    SETUP AND ASSETS

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class BoardManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
